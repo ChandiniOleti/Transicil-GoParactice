4e8bec15fd49aff437c2433d5f9c45793c80b639
Chandini Oleti <oletichandini78@gmail.com>
2026-08-13 04:36:05 +0000

    React practice on some basics

---
+++ React/react-basics/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>PayLater</h1>
+      <h2>Welcome Chandini</h2>
+      <p>You are currently logged out.</p>
+    </div>
+  )
+}
+
+export default App
